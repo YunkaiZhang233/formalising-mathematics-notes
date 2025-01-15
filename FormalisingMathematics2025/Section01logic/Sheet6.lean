0c12661e891da9e36b55c1e612e58bfc4c8bf36e
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2025-01-15 13:43:12 +0000

    [exercise] finished sec1 sheet 6

--- FormalisingMathematics2025/Section01logic/Sheet6.lean
+++ FormalisingMathematics2025/Section01logic/Sheet6.lean
@@ -33,43 +33,121 @@ example : P → P ∨ Q := by
   done
 
 example : Q → P ∨ Q := by
-  sorry
+  intro hQ
+  right
+  exact hQ
   done
 
+-- inlining
 example : P ∨ Q → (P → R) → (Q → R) → R := by
   intro hPoQ
+  intro hPR hQR
   cases hPoQ with
-  | inl h => sorry
-  | inr h => sorry
+  | inl h => apply hPR; assumption
+  | inr h => apply hQR; assumption
   done
 
 -- symmetry of `or`
 example : P ∨ Q → Q ∨ P := by
-  sorry
+  intro h
+  cases h
+  case inl hP => right; assumption
+  case inr hP => left; assumption
   done
 
 -- associativity of `or`
 example : (P ∨ Q) ∨ R ↔ P ∨ Q ∨ R := by
-  sorry
+  constructor <;> intro h
+  -- case 1: (P V Q) V R -> P V (Q v R)
+  · cases' h with h1 h2
+    · cases h1
+      case inl hP => left; exact hP
+      case inr hQ => right; left; exact hQ
+    · right; right; exact h2
+  -- case 2: P V (Q V R) -> (P V Q) v R
+  · cases' h with h1 h2
+    · left; left; exact h1
+    · cases h2
+      case inl hQ => left; right; exact hQ
+      case inr hR => right; exact hR
   done
 
 example : (P → R) → (Q → S) → P ∨ Q → R ∨ S := by
-  sorry
+  intro hPR hQS h
+  cases h
+  case inl hP =>
+    left
+    apply hPR
+    exact hP
+  case inr hQ =>
+    right
+    apply hQS
+    exact hQ
   done
 
 example : (P → Q) → P ∨ R → Q ∨ R := by
-  sorry
+  intro hPQ hPR
+  cases hPR
+  case inl hP =>
+    left
+    apply hPQ
+    assumption
+  case inr hR =>
+    right
+    exact hR
   done
 
 example : (P ↔ R) → (Q ↔ S) → (P ∨ Q ↔ R ∨ S) := by
-  sorry
+  intro hPR hQS
+  rw [hPR, hQS]
   done
 
 -- de Morgan's laws
 example : ¬(P ∨ Q) ↔ ¬P ∧ ¬Q := by
-  sorry
+  constructor <;> intro h
+  · constructor
+    · change P → False
+      intro hP
+      have hPQ : P ∨ Q := by left; exact hP
+      apply h
+      exact hPQ
+    · change Q → False
+      intro hQ
+      change P ∨ Q -> False at h
+      apply h
+      right
+      exact hQ
+  · change P ∨ Q → False
+    intros hPQ
+    cases' h with hnP hnQ
+    cases hPQ
+    case inl hP => apply hnP; exact hP
+    case inr hQ => apply hnQ; exact hQ
   done
 
 example : ¬(P ∧ Q) ↔ ¬P ∨ ¬Q := by
-  sorry
+  constructor <;> intro h
+  · -- left
+    by_cases hP : P
+    · -- P holds
+      by_cases hQ : Q
+      · -- Q holds
+        exfalso
+        have hPQ : P ∧ Q := by constructor <;> assumption
+        change P ∧ Q → False at h
+        apply h at hPQ
+        exact hPQ
+      · -- not Q holds
+        right
+        exact hQ
+    · -- not P holds
+      left
+      exact hP
+  · -- right
+    change P ∧ Q → False
+    intro hPQ
+    cases' hPQ with hP hQ
+    cases h with
+    | inl hnP => apply hnP; exact hP
+    | inr hnQ => apply hnQ; exact hQ
   done
